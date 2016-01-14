94126b50b543b1c266b377ee58cc934d8ff5f374
Jayakrishna-1996 <krishnajasti1996@gmail.com>
2016-01-14 14:45:35 +0530

    sv.co source code

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+add_filter( 'woocommerce_currencies', 'add_my_currency' );
+
+function add_my_currency( $currencies ) {
+     $currencies['ABC'] = __( 'Currency name', 'woocommerce' );
+     return $currencies;
+}
+
+add_filter('woocommerce_currency_symbol', 'add_my_currency_symbol', 10, 2);
+
+function add_my_currency_symbol( $currency_symbol, $currency ) {
+     switch( $currency ) {
+          case 'ABC': $currency_symbol = '$'; break;
+     }
+     return $currency_symbol;
+}
+/**
+ * Main file
+ */
+
+/**
+ * Initialize all the things. 
+ */
+require get_template_directory() . '/inc/init.php';
+
+/**
+ * Note: Do not add any custom code here. Please use a child theme so that your customizations aren't lost during updates.
+ * http://codex.wordpress.org/Child_Themes
+ */
+
+
+ require 'inc/cwp-update-free.php'; 
+
